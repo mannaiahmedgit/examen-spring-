3b24c261b23d4870879ff496d074d4a751fa06f8
mannaiahmedgit <mannai.ahmed5555@gmail.com>
2022-02-14 16:54:14 +0100

    add configuration Security 2

--- src/main/java/tn/suptech/examenahmedmannainumero_sujetv/security/SecurityConfig.java
+++ src/main/java/tn/suptech/examenahmedmannainumero_sujetv/security/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
        http.authorizeRequests().anyRequest().authenticated();
 
 
+
     }
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
